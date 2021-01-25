f559487fe27e1854adb12b8bc77e84c0b1094c37
Mandy <drgnfli6420@yahoo.com>
2021-01-25 08:27:05 -0500

    set up plants

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,15 @@
 console.log("Welcome to the main module")
 
+import { createPlan } from './plan.js'
+import { createAsparagus } from "./seeds/asparagus.js"
+
+
+// Remember that to assign what a function returns as a value of a variable, the syntax is...
+
+// const appropriatelyNamedVariable = theFunctionThatReturnsAValue()
+
+const yearlyPlan = createPlan();
+console.log(yearlyPlan)
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
